e2b3db390d34b7ea75b84d30b7c1c3f500df3816
Binh Nguyen <binhnguyenthanh2020@gmail.com>
2024-05-20 13:28:36 -0600

    Update findElement function

--- src/main/java/common/Utilities.java
+++ src/main/java/common/Utilities.java
@@ -3,25 +3,17 @@ package common;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 
 public class Utilities {
     public static WebElement findElement(By locator){
         try{
-            WebElement element = (new WebDriverWait(Constants.driver, Duration.ofSeconds(Constants.TIMEOUT)))
-                    .until(ExpectedConditions.presenceOfElementLocated(locator));
-            return element.findElement(locator);
+            WebElement element = Constants.driver.findElement(locator);
+            return element.isDisplayed() ? element : null;
         } catch (NoSuchElementException e){
-            e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
-    public static Boolean waitForElementToDisappear(WebElement element){
-        return new WebDriverWait(Constants.driver, Duration.ofSeconds(Constants.TIMEOUT)).until(ExpectedConditions.invisibilityOf(element));
-    }
 
 }
