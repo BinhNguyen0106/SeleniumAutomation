8a8f61975fb8593c78ec1fe3a0c3be617c05e129
Binh Nguyen <binhnguyenthanh2020@gmail.com>
2024-05-21 08:50:35 -0600

    Add method scrollInToView using JavascriptExecutor RandomNumber using Random findElements

--- src/main/java/common/Utilities.java
+++ src/main/java/common/Utilities.java
@@ -1,9 +1,13 @@
 package common;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+import java.util.Random;
 
 
 public class Utilities {
@@ -16,9 +20,28 @@ public class Utilities {
         }
     }
 
+    public static List<WebElement> findElements(By locator){
+        try {
+            List<WebElement> elements = Constants.DRIVER.findElements(locator);
+            return (elements.size() != 0) ? elements : null;
+        } catch (NoSuchElementException e){
+            return null;
+        }
+    }
+
     public static void hoverToElement(WebElement element){
         Actions actions = new Actions(Constants.DRIVER);
         actions.moveToElement(element).perform();
+    }
+
+    public static int randomNumber(int min, int max){
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
+    }
+
+    public static void scrollInToView(WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor) Constants.DRIVER;
+        js.executeScript("arguments[0].scrollIntoView();", element);
     }
 
 }
